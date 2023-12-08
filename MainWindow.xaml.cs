af93be92f36a9235fd190fed2eeadf5ff26f8202
denysyukkk <149053076+denysyukkk@users.noreply.github.com>
2023-12-08 03:07:01 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System;
+using System.Windows;
+
+namespace QuadraticEquationSolverWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SolveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(coeffATextBox.Text, out double a) ||
+                !double.TryParse(coeffBTextBox.Text, out double b) ||
+                !double.TryParse(coeffCTextBox.Text, out double c))
+            {
+                resultLabel.Content = "Некоректні коефіцієнти. Будь ласка, введіть дійсні числа.";
+                return;
+            }
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+
+                resultLabel.Content = $"Дискримінант: {discriminant}\nРозв'язки: {root1}, {root2}";
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                resultLabel.Content = $"Дискримінант: {discriminant}\nРозв'язок: {root}";
+            }
+            else
+            {
+                resultLabel.Content = "Розв'язків немає.";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Оберіть, яке значення обчислити:");
+        Console.WriteLine("1. Сума 1^n + 2^(n/2) + ... + k^(n/k)");
+        Console.WriteLine("2. Сума 1^k + 2^k + ... + n^k");
+        Console.WriteLine("3. Сума (1/n) + (2/n^2) + (3/n^3) + ... + (k/n^k)");
+
+        if (!int.TryParse(Console.ReadLine(), out int choice))
+        {
+            Console.WriteLine("Некоректний вибір. Будь ласка, введіть число від 1 до 3.");
+            return;
+        }
+
+        Console.WriteLine("Введіть значення n:");
+        if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+        {
+            Console.WriteLine("Некоректне значення n. Будь ласка, введіть ціле число більше 0.");
+            return;
+        }
+
+        Console.WriteLine("Введіть значення k:");
+        if (!int.TryParse(Console.ReadLine(), out int k) || k <= 0)
+        {
+            Console.WriteLine("Некоректне значення k. Будь ласка, введіть ціле число більше 0.");
+            return;
+        }
+
+        double result = 0;
+
+        switch (choice)
+        {
+            case 1:
+                result = CalculateSum1(n, k);
+                break;
+            case 2:
+                result = CalculateSum2(n, k);
+                break;
+            case 3:
+                result = CalculateSum3(n, k);
+                break;
+            default:
+                Console.WriteLine("Некоректний вибір. Будь ласка, введіть число від 1 до 3.");
+                return;
+        }
+
+        Console.WriteLine($"Результат обчислення: {result}");
+    }
+
+    static double CalculateSum1(int n, int k)
+    {
+        double sum = 0;
+        for (int i = 1; i <= k; i++)
+        {
+            sum += Math.Pow(i, n / (double)i);
+        }
+        return sum;
+    }
+
+    static double CalculateSum2(int n, int k)
+    {
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += Math.Pow(i, k);
+        }
+        return sum;
+    }
+
+    static double CalculateSum3(int n, int k)
+    {
+        double sum = 0;
+        for (int i = 1; i <= k; i++)
+        {
+            sum += i / Math.Pow(n, i);
+        }
+        return sum;
+    }
+}
+
